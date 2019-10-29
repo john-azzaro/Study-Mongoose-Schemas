7ad1c0603da236ab1f6fea72019fd2f3fc9e5c94
John Azzaro <jazzaro@temple.edu>
2019-10-29 09:54:42 -0700

    updated model example

--- modelsExample.js
+++ modelsExample.js
@@ -1,23 +1,24 @@
 /* 
-In the following example, we create a basic model for a book
+In the following example, the objective is to create a model of a Car such that the schema that all documents 
+in our database will have will have make, model, year, and rating.
 
 */
 
 
 
-const mongoose = required('mongoose');               // Import Mongoose 
+const mongoose = required('mongoose');                    // Import Mongoose 
 
-const bookSchema = new mongoose.Schema({             // New schema of Book
-    title: String,
-    author: String,
-    summary: String,
-    isbn: Number
+const carSchema = new mongoose.Schema({                   // New schema of carSchema
+    make: String,
+    model: String,
+    year: Number,
+    rating: String
 });
 
-const Book = mongoose.model('Book', bookSchema);     // Create the "Book" model by creating a model with the collection name in 
-                                                     // corresponding MongoDB database (e.g. Book) and the corresponding schema (bookSchema).
+const Car = mongoose.model('Car', carSchema);             // Create the "Car" model by creating a model with the collection name in 
+                                                          // corresponding MongoDB database (e.g. Car) and the corresponding schema (carSchema).
 
-module.exports = { Book };                           // Export the models on your page via destructured object. 
-                                                     // IN this case, we only have one model... Book.
+module.exports = { Car };                                 // Export the models on your page via destructured object. 
+                                                          // In this case, we only have one model... Car.
 
 
